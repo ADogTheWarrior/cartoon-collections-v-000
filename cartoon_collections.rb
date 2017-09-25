fcc1a1dc97e8bb7830ab3addfca3c1192c9f27cc
Adam Fudge <coolfudge@gmail.com>
2017-09-25 23:13:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,14 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  output = true
+  array.each do |word|
+    if word.length > 4
+      output = false
+    end
+  end
+  output
 end
 
 def find_the_cheese# code an argument here
